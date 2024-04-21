5e5da1b12d8bb2d82dca32b923623aa68ca0f038
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-04-21 19:44:25 -0400

    Update `package-lock.json` upon exit

--- bin/sapm.js
+++ bin/sapm.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
 
+const { execSync } = require('child_process');
 const { parseArgs } = require('util');
 
 
 const SAPM = require('..');
 
 
+let sapm = null;
+
+
 const PARSE_ARGS_OPTIONS = {
+    'package-lock-only': {
+        type: 'boolean',
+        default: false
+    },
     'save-dev': {
         type: 'boolean',
         short: 'D',
@@ -90,20 +98,43 @@ function logSubcommandUsage (subcommand) {
 
 
 async function install (
-    options,
+    options = {},
     ...packageNames
 ) {
-    const sapm = new SAPM({ nodeModulesPath: 'plugins' });
+    const packageLockOnly = options['package-lock-only'];
+
+    // If `sapm i --package-lock-only` was called,
+    // then update `package-lock.json` and exit early.
+    if (packageLockOnly) {
+        updatePackageLockJSON();
+        exit();
+    }
+
+    const packagePath = options.cwd ?? process.cwd();
+
+    sapm = new SAPM(
+        packagePath,
+        {
+            installPath: options.installPath ?? null
+        }
+    );
 
     await sapm.install(...packageNames);
 }
 
 
 async function uninstall (
-    options,
+    options = {},
     ...packageNames
 ) {
-    const sapm = new SAPM({ nodeModulesPath: 'plugins' });
+    const packagePath = options.cwd ?? process.cwd();
+
+    sapm = new SAPM(
+        packagePath,
+        {
+            installPath: options.installPath ?? null
+        }
+    );
 
     await sapm.uninstall(...packageNames);
 }
@@ -122,7 +153,7 @@ async function runSubcommand (
 
         case 'i':
         case 'install': {
-            const options = args.options;
+            const options = args.values;
             const packageNames = args.positionals;
 
             await install(
@@ -135,7 +166,7 @@ async function runSubcommand (
 
         case 'un':
         case 'uninstall': {
-            const options = args.options;
+            const options = args.values;
             const packageNames = args.positionals;
 
             await uninstall(
@@ -145,6 +176,22 @@ async function runSubcommand (
 
             return;
         }
+    }
+}
+
+
+async function exit (code = 0) {
+    updatePackageLockJSON();
+
+    process.exit(code);
+}
+
+
+async function updatePackageLockJSON () {
+    try {
+        execSync("npm i --package-lock-only");
+    } catch {
+        execSync("sapm i --package-lock-only");
     }
 }
 
@@ -164,6 +211,8 @@ async function main (args) {
         subcommand,
         args
     );
+
+    exit();
 }
 
 
