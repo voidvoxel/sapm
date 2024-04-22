34e564d3f8e1694e1d3f47cf607b46a5aa1ef881
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-04-22 06:25:41 -0400

    Add comments mapping functions to commands

--- examples/install-and-use.js
+++ examples/install-and-use.js
@@ -2,16 +2,22 @@ const SAPM = require("../src");
 
 
 async function main () {
+    // Create a new package.
+    // Equivalent command:
+    // cd tmp/app && npm init -y
     const sapm = new SAPM("tmp/app");
 
+    // Install dependency `moment`.
+    // Equivalent command:
+    // sapm install moment
     const packageName = 'moment';
-
     await sapm.install(packageName);
 
+    // Require and use the module.
+    // Equivalent command:
+    // node -e "console.log( require('moment').now() );"
     const package = sapm.require(packageName);
-
     const time = package.now();
-
     console.log('time:', time);
 }
 
--- examples/install-multiple.js
+++ examples/install-multiple.js
@@ -2,12 +2,18 @@ const SAPM = require("../src");
 
 
 async function main () {
+    // Create a new package.
+    // Equivalent command:
+    // cd tmp/app && npm init -y
     const sapm = new SAPM("tmp/app");
 
+    // Install `moment`, `block-stream`, and `@voidvoxel/position-3d`.
+    // Equivalent command:
+    // sapm install moment block-stream @voidvoxel/position-3d
     await sapm.install(
         'moment',
-        '@voidvoxel/position-3d',
-        'block-stream'
+        'block-stream',
+        '@voidvoxel/position-3d'
     );
 }
 
